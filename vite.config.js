0c88e1bd4541354d8360f6e8325610df95e4d3ad
Abdullah Bin Mansoor <mabm322005@gmail.com>
2026-03-17 07:37:05 +0500

    Update base path in vite.config.js
    Set base path to '/' for both development and production.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   // Use '/' for development ('serve'), '/portfolio-website/' for production build
-  base: command === 'serve' ? '/' : '/portfolio-website/',
+  base: '/',
   build: {
     target: 'es2020',
     minify: 'terser',
